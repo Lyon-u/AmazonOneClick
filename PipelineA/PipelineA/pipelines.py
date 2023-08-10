44cc15cb9c7003574538675918ed551c381de09a
Lyon <lion.yu@strikingly.com>
2023-08-10 14:49:30 +0800

    init

---
+++ PipelineA/PipelineA/pipelines.py
@@ -0,0 +1,30 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+import random
+import string
+
+
+class GenerateID:
+    def process_item(self, item, spider):
+        task_id = GenerateID.generate_id()
+        item['task_id'] = task_id
+        return item
+
+    @staticmethod
+    def generate_id():
+        characters = string.ascii_letters + string.digits  # 包含所有字母和数字的字符串
+        task_id = ''.join(random.choices(characters, k=8))  # 随机选择8个字符并拼接成字符串
+        return task_id
+
+
+class AmazononeclickPipeline:
+    def process_item(self, item, spider):
+        print(item)
+        return item
